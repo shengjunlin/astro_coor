417df2bb8f1e373d45372890e826b982ba3b1ce9
Sheng-Jun <sebastian0117@gmail.com>
2017-08-16 22:38:51 +0800

    Upload the module astro_coor.py and RADec_convert.py

---
+++ RADec_convert.py
@@ -0,0 +1,257 @@
+#! /usr/bin/env python
+#-*- coding:utf-8 -*-
+# Written on 6/14/2014 by Sheng-Jun Lin
+# 8/10/2014: Add Angular Distance Mode
+# 8/15/2017: Move classes to be the modulo astro_coor
+
+from astro_coor import *
+from sys import argv
+from getopt import getopt
+from subprocess import call, PIPE, Popen
+
+def resolve_RA_line(string, line):
+
+    try:
+        RA = resolve_RA(string)
+        return RA
+    except ValueError as err_msg:
+        raise ValueError('RADec_convert: [line: {0}] {1}'.format(line, str(err_msg)))
+
+
+def resolve_Dec_line(string, line):
+
+    try:
+        Dec = resolve_Dec(string)
+        return Dec
+    except ValueError as err_msg:
+        raise ValueError('RADec_convert: [line: {0}] {1}'.format(line, str(err_msg)))
+
+def main():
+
+    help_doc = 'RADec_convert.py: \n\
+A Python script to calculate all the RA/Dec forms, the angular distance,\n\
+                   or the angular displaccement of the coordinates.\n\n\
+Usage: \n\
+1) Input from a file:           python RADec_convert.py [OPTION]...  [INPUT FILE]\n\
+2) Input from the command line: python RADec_convert.py -c [OPTION].. [COORDINATES]...\n\
+  -o output_file Specify the output filename. Otherwise it will be named as RADec.output\n\
+         If the file already exists, the result will be appended to the file.\n\
+  -d             Change to [Angular Distance Mode].\n\
+  -x             Change to [Angular Displacement Mode].\n\
+  -c             Don\'t assign a input file but typing in comand line directly.\n\
+  -n             Don\'t generate the output file.\n\
+  -s             Don\'t print the result on screen.\n\
+  -h             Print this help information.\n\n\
+Input file format:\n\
+ [Converter Mode] (the default process)\n\
+  For RA, there are 3 forms to choose.\n\
+    1) hour(int) : min(int)   : sec(float)\n\
+    2) deg(int)  : arcmin(int): arcsec(float)\n\
+    3) degree (float)\n\
+  For Dec, there are 2 forms to choose.\n\
+    1) +/-deg(int) : arcmin(int): arcsec(float), where \'+\' could be ignored.\n\
+    2) degree (float)\n\
+  Please write coordinates into each line,\n\
+  where the 1st component is RA, and the 2nd one is Dec.\n\
+  For the 2nd form of RA, you need to prefix a keyword \'d\' without space.\n\
+  For integer vars hour/min/deg/arcmin, you could input float vars,\n\
+  then the decimal part will be combined to the next subunit.\n\
+  And the line which starts from \'#\' will be ignored.\n\
+  Example:\n\
+           10:23:47.68  0:38:41.3\n\
+           155.948674   00.644805\n\
+           d155:56:55.2 -0:38:41.3\n\
+ [Angular Distance Mode]\n\
+  The Format: RA(1st)  Dec(1st)  RA(2nd)  Dec(2nd)\n\
+  For each line, please write the coordinates of 2 points, in whatever forms below.\n\
+  Example:\n\
+           10:23:47.68  0:38:41.3  10:01:55  0:30:40\n\
+ [Angular Displacement Mode]\n\
+  The Format: RA  Dec  displacement(the default is in arcsec)  P.A.(the default is in deg)\n\
+  For each line, please write the coordinates of a point, in whatever forms below.\n\
+  Then write the displacement, and the position angle.\n\
+  One can use \'arcsec\', \'as\', \'arcmin\', \'am\', \'deg\', \'d\' to change the unit.\n\
+  Example:\n\
+           10:23:47.68  0:38:41.3  328.251247arcmin -91.371639\n\n\
+Output file format:\n\
+ The 1st line is the time of executing this command.\n\
+ [Converter Mode] List all forms of input coordinates.\n\
+ [Angular Distance Mode]\n\
+  List 2 coordinates in hms and dms form,\n\
+  the angular distance in arcsec, arcmin, and degree, and the position angle of them.\n\
+ [Angular Displacement Mode]\n\
+  List all forms of input and output coordinates,\n\
+  and the angular distance in arcsec, arcmin, and degree.'
+
+    opts, args = getopt(argv[1:], "ho:nsdxc")
+
+    exec_cmdline = False
+    on_screen = True
+    gen_file = True
+    distanceMode = False
+    displacementMode = False
+    outputfile = 'RADec.output'
+
+    for opt, value in opts:
+        if opt == '-h':
+            print(help_doc)
+            exit()
+        if opt == '-o':
+            outputfile = value
+        if opt == '-s':
+            on_screen = False
+        if opt == '-n':
+            gen_file = False
+        if opt == '-d':
+            distanceMode = True
+        if opt == '-x':
+            displacementMode = True
+        if opt == '-c':
+            exec_cmdline = True
+
+    if gen_file:
+        cmd = Popen(['ls'], stdout=PIPE)
+        shell_result = cmd.communicate()[0]
+        if outputfile not in shell_result:
+            call(['touch', outputfile])
+            txt = open(outputfile, 'a')
+            call(['date'], stdout=txt, shell=True)
+        else:
+            txt = open(outputfile, 'a')
+            call(['date'], stdout=txt, shell=True)
+    if exec_cmdline:
+        items = args
+        database = [1]
+    else:
+        inputfile = open(args[0], 'r')
+        database = inputfile.readlines()
+        inputfile.close()
+
+    n = 1
+    result = ''
+    for i in xrange(len(database)):
+        # Reading the input coordinates
+        if not exec_cmdline:
+            items = database[i].split()
+        if len(items) == 0 or '#' in items[0]:
+            continue
+        data_ra1 = items[0]
+        data_dec1 = items[1]
+
+        # Resolving the input coordinates
+        RA1 = resolve_RA_line(data_ra1, i + 1)
+        Dec1 = resolve_Dec_line(data_dec1, i + 1)
+
+        # Calculating all the representation of input coordinates
+        RA1.converter()
+        Dec1.converter()
+
+        if distanceMode:
+            # For calculating angular distance
+            data_ra2 = items[2]
+            data_dec2 = items[3]
+            RA2 = resolve_RA_line(data_ra2, i + 1)
+            Dec2 = resolve_Dec_line(data_dec2, i + 1)
+            RA2.converter()
+            Dec2.converter()
+            [pa21, pa12, d] = DistanceMode(RA1, Dec1, RA2, Dec2)
+
+            # Write into the outputfile [anglar distance]
+            result += ('#{0:-<4d}--{1:-<12s}--{2:-<14s} '
+                       '{3:>15s} {4:>15s} {5:>15s}\n'.format(
+                       n, 'R.A.', 'Dec.', 'dist["]', "dist[']", 'dist[deg]'))
+            n += 1
+            result += ('1)hms {0:>2d}:{1:0>2d}:{2:0>7.4f}  '
+                       '{3}{4}:{5:0>2d}:{6:0>7.4f} '
+                       '{7:15f} {8:15f} {9:15f}\n'.format(
+                       RA1.h, RA1.m, RA1.s,
+                       Dec1.sign, Dec1.d, Dec1.arcm, Dec1.arcs,
+                       d * 3600., d * 60., d))
+            result += ('2)hms {0:>2d}:{1:0>2d}:{2:0>7.4f}  '
+                       '{3}{4}:{5:0>2d}:{6:0>7.4f}  '
+                       'P.A.[deg]={7:+11.6f}(2wrt1)/{8:+11.6f}(1wrt2)\n'.format(
+                       RA2.h, RA2.m, RA2.s,
+                       Dec2.sign, Dec2.d, Dec2.arcm, Dec2.arcs, pa21, pa12))
+        elif displacementMode:
+            # For calculating angular displacement
+            x = items[2]
+            pa = items[3]
+            if x[-1] == 'd':
+                x = float(x[:-1]) * 3600
+            elif x[-3:] == 'deg':
+                x = float(x[:-3]) * 3600
+            elif x[-2:] == 'am':
+                x = float(x[:-2]) * 60.
+            elif x[-6:] == 'arcmin':
+                x = float(x[:-6]) * 60.
+            elif x[-2:] == 'as':
+                x = float(x[:-2])
+            elif x[-6:] == 'arcsec':
+                x = float(x[:-6])
+            else:
+                x = float(x)
+            if pa[-2:] == 'as':
+                pa = float(pa[:-2]) / 3600.
+            elif pa[-6:] == 'arcsec':
+                pa = float(pa[:-6]) / 3600.
+            elif pa[-2:] == 'am':
+                pa = float(pa[:-2]) / 60.
+            elif pa[-6:] == 'arcmin':
+                pa = float(pa[:-6]) / 60.
+            elif pa[-1] == 'd':
+                pa = float(pa[:-1])
+            elif pa[-3:] == 'deg':
+                pa = float(pa[:-3])
+            else:
+                pa = float(pa)
+            [RA2, Dec2] = DisplacementMode(RA1, Dec1, x, pa)
+            [pa21, pa12, d] = DistanceMode(RA1, Dec1, RA2, Dec2)
+
+            # Write into the outputfile [anglar displacement]
+            result += ('#{0:-<4d}--{1:-<12s}--{2:-<14s} '
+                       '{3:>15s} {4:>15s} {5:>15s}\n'.format(
+                       n, 'R.A.', 'Dec.', 'dist["]', "dist[']", 'dist[deg]'))
+            n += 1
+            result += ('1)hms {0:>2d}:{1:0>2d}:{2:0>7.4f}  '
+                       '{3}{4}:{5:0>2d}:{6:0>7.4f} '
+                       '{7:15f} {8:15f} {9:15f}\n'.format(
+                       RA1.h, RA1.m, RA1.s,
+                       Dec1.sign, Dec1.d, Dec1.arcm, Dec1.arcs,
+                       x, x / 60., x / 3600))
+            result += ('  dms {0:>3d}:{1:0>2d}:{2:0>7.4f}                '
+                       'P.A.[deg]={3:+11.6f}(2wrt1)/{4:+11.6f}(1wrt2)\n'.format(
+                       RA1.d, RA1.arcm, RA1.arcs, pa21, pa12))
+            result += '  deg {0:>14.10f}  {1:+.10f}\n'.format(
+                      RA1.all_d, Dec1.all_d)
+            result += ('2)hms  {0:>2d}:{1:0>2d}:{2:0>7.4f}  '
+                       '{3}{4:0>2d}:{5:0>2d}:{6:0>7.4f}\n'.format(
+                       RA2.h, RA2.m, RA2.s,
+                       Dec2.sign, Dec2.d, Dec2.arcm, Dec2.arcs))
+            result += '  dms {0:>3d}:{1:0>2d}:{2:0>7.4f}\n'.format(
+                       RA2.d, RA2.arcm, RA2.arcs)
+            result += '  deg {0:>14.10f}  {1:+.10f}\n'.format(
+                       RA2.all_d, Dec2.all_d)
+
+        else:
+            # Write into the outputfile [coordinates converter]
+            result += '#{0:-<3d}--{1:-<12s}----{2:-<1s}\n'.format(
+                      n, 'R.A.', 'Dec.')
+            n += 1
+            result += ('hms  {0:>2d}:{1:0>2d}:{2:0>7.4f}  '
+                       '{3}{4:0>2d}:{5:0>2d}:{6:0>7.4f}\n'.format(
+                       RA1.h, RA1.m, RA1.s,
+                       Dec1.sign, Dec1.d, Dec1.arcm, Dec1.arcs))
+            result += 'dms {0:>3d}:{1:0>2d}:{2:0>7.4f}\n'.format(
+                       RA1.d, RA1.arcm, RA1.arcs)
+            result += 'deg {0:>14.10f}  {1:+.10f}\n'.format(
+                       RA1.all_d, Dec1.all_d)
+    result += '=' * 35 + '\n'
+    if gen_file:
+        txt.write(result + '\n')
+        txt.close()
+    if on_screen:
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
---
+++ astro_coor.py
@@ -0,0 +1,523 @@
+#! /usr/bin/env python
+#-*- coding:utf-8 -*-
+# Written on 6/14/2014 by Sheng-Jun Lin
+# 8/15/2017: Rewrite the method to classify types of coor. forms
+
+import numpy as np
+from functools import total_ordering
+import warnings
+import sys
+
+deg2rad = np.pi / 180.
+rad2deg = 180. / np.pi
+
+@total_ordering
+class ra(object):
+
+    '''
+    ra class stores the RA part of coordinates.
+    RA forms: h:m:s / d:arcm:arcs / degree(all_d)
+    Any one of forms is acceptable for creating a ra class
+    since ra.converter() automatically calculates other forms.
+        e.g. ra(h=4, m=31, s=30), ra(d=67, arcm=52, arcs=30),
+             or ra(all_d=67.875)
+    While print a ra class, only the hms form will be returned.
+    ra classes are ordered,
+        i.e. the operators: '<', '>', '==', and etc. are valid.
+    '''
+    def __init__(self, h=np.nan, m=np.nan, s=np.nan,
+                 d=np.nan, arcm=np.nan, arcs=np.nan, all_d=np.nan):
+        self.h = float(h)  # hour
+        self.m = float(m)  # minute
+        self.s = float(s)  # second
+        self.d = float(d)  # deg
+        self.arcm = float(arcm)  # arcmin
+        self.arcs = float(arcs)  # arcsec
+        self.all_d = float(all_d)  # all in deg
+        self.priority = 0
+
+    def __eq__(self, other):
+        self.converter()
+        other.converter()
+        return self.all_d == other.all_d
+
+    def __lt__(self, other):
+        self.converter()
+        other.converter()
+        return self.all_d < other.all_d
+
+    def __repr__(self):
+        self.converter()
+        return '{0}:{1}:{2}'.format(self.h, self.m, self.s)
+
+    def set_h(self, h):
+        self.h = int(h)  # hour
+        self.priority = 1
+
+    def set_m(self, m):
+        self.m = int(m)  # minute
+        self.priority = 1
+
+    def set_s(self, s):
+        self.s = float(s)  # second
+        self.priority = 1
+
+    def set_d(self, d):
+        self.d = int(d)  # deg
+        self.priority = 2
+
+    def set_arcm(self, arcm):
+        self.arcm = int(arcm)  # arcmin
+        self.priority = 2
+
+    def set_arcs(self, arcs):
+        self.arcs = float(arcs)  # arcsec
+        self.priority = 2
+
+    def set_all_d(self, all_d):
+        self.all_d = float(all_d)  # all in deg
+        self.priority = 3
+
+    def unit(self):
+        if self.priority == 0:
+            if (not np.isnan(self.h)) and (not np.isnan(self.m)) \
+                    and (not np.isnan(self.s)):
+                self.priority = 1  # hour-min-sec system
+            elif (not np.isnan(self.d)) and (not np.isnan(self.arcm)) \
+                    and (not np.isnan(self.arcs)):
+                self.priority = 2  # deg-arcmin-arcsec system
+            elif not np.isnan(self.all_d):
+                self.priority = 3  # degree system
+            else:
+                raise ValueError('RA doesn\'t be assigned completely.')
+
+    def hms2all_d(self):
+        if self.h == np.nan or self.h < 0:
+            raise ValueError("The hms form of RA isn't assigned completely!")
+        all_d = self.h * 15.
+        all_d += self.m * 0.25
+        all_d += self.s * 0.25 / 60.
+        self.set_all_d(all_d)
+
+    def all_d2hms(self):
+        if self.all_d == np.nan or self.all_d < 0:
+            raise ValueError("The degree form of RA isn't assigned completely!")
+        self.set_h(int(self.all_d / 15.))
+        self.set_m(int((self.all_d - self.h * 15.) / 0.25))
+        self.set_s((self.all_d - self.h * 15. - self.m * 0.25) / (0.25 / 60.))
+
+    def dms2all_d(self):
+        if self.d == np.nan or self.d < 0:
+            raise ValueError("The dms form of RA isn't assigned completely!")
+        all_d = self.d
+        all_d += self.arcm / 60.
+        all_d += self.arcs / 3600.
+        self.set_all_d(all_d)
+
+    def all_d2dms(self):
+        if self.all_d == np.nan or self.all_d < 0:
+            raise ValueError("The degree form isn't assigned completely!")
+        self.set_d(int(self.all_d))
+        self.set_arcm(int((self.all_d - self.d) * 60.))
+        self.set_arcs((self.all_d - self.d) * 3600. - self.arcm * 60.)
+
+    def converter(self):
+        if self.priority == 0:
+            self.unit()
+        if self.priority == 1:
+            # The h:m:s form exists
+            self.hms2all_d()
+            self.all_d2dms()
+            # If h and m in h:m:s are not integers, recalc the h:m:s form.
+            h = self.h
+            m = self.m
+            if h % 1 != 0. or m % 1 != 0.:
+                self.all_d2hms()
+            else:
+                self.set_h(int(h))
+                self.set_m(int(m))
+        elif self.priority == 2:
+            # The d:m:s form exists
+            self.dms2all_d()
+            self.all_d2hms()
+            # If d and m in d:m:s are not integers, recalc the d:m:s form.
+            d = self.d
+            arcm = self.arcm
+            if d % 1 != 0. or arcm % 1 != 0.:
+                self.all_d2dms()
+            else:
+                self.set_d(int(d))
+                self.set_arcm(int(arcm))
+        else:
+            # The degree form exists
+            self.all_d2hms()
+            self.all_d2dms()
+
+
+@total_ordering
+class dec(object):
+
+    '''
+    dec class stores the Dec part of coordinates.
+    Dec forms: d:arcm:arcs / degree(all_d)
+    Any one of forms is acceptable for creating a dec class
+    since dec.converter() automatically calculates other forms.
+        e.g. dec(sign='+', d=18, arcm=12, arcs=30),
+             or dec(all_d=18.208333)
+    Note that d is always nonagtive.
+    While print a dec class, only the dms form will be returned.
+    dec classes are ordered,
+        i.e. the operators: '<', '>', '==', and etc. are valid.
+    '''
+    def __init__(self, sign='+', d=np.nan, arcm=np.nan, arcs=np.nan,
+                 all_d=np.nan):
+        self.sign = sign  # positive/negative ('+'/'-')
+        self.d = float(d)  # deg
+        self.arcm = float(arcm)  # arcmin
+        self.arcs = float(arcs)  # arcsec
+        self.all_d = float(all_d)  # all in deg
+        self.priority = 0
+
+    def __eq__(self, other):
+        self.converter()
+        other.converter()
+        return self.all_d == other.all_d
+
+    def __lt__(self, other):
+        self.converter()
+        other.converter()
+        return self.all_d < other.all_d
+
+    def __repr__(self):
+        self.converter()
+        return '{0}{1}:{2}:{3}'.format(self.sign, self.d, self.arcm, self.arcs)
+
+    def set_sign(self, pn):
+        self.sign = pn  # positive/negative
+        self.priority = -2
+
+    def set_d(self, d):
+        self.d = int(d)  # deg
+        self.priority = -2
+
+    def set_arcm(self, arcm):
+        self.arcm = int(arcm)  # arcmin
+        self.priority = -2
+
+    def set_arcs(self, arcs):
+        self.arcs = float(arcs)  # arcsec
+        self.priority = -2
+
+    def set_all_d(self, all_d):
+        self.all_d = float(all_d)  # all in deg
+        self.priority = -3
+
+    def unit(self):
+        if self.priority == 0:
+            if (not np.isnan(self.d)) and (not np.isnan(self.arcm)) \
+                    and (not np.isnan(self.arcs)):
+                self.priority = -2  # deg-arcmin-arcsec system
+            elif not np.isnan(self.all_d):
+                self.priority = -3  # degree sytem
+            else:
+                raise ValueError('Dec doesn\'t be assigned completely.')
+
+    def dms2all_d(self):
+        if self.d == np.nan:
+            raise ValueError("The dms form of Dec isn't assigned completely!")
+        all_d = self.d
+        all_d += self.arcm / 60.
+        all_d += self.arcs / 3600.
+        if self.sign == '-':
+            all_d *= -1
+        self.set_all_d(all_d)
+
+    def all_d2dms(self):
+        if self.all_d == np.nan:
+            raise ValueError(
+                    "The degree form of Dec isn't assigned completely!")
+        if self.all_d < 0:
+            self.sign = '-'
+        else:
+            self.sign = '+'
+        abs_all_d = abs(self.all_d)
+        self.set_d(int(abs_all_d))
+        self.set_arcm(int((abs_all_d - self.d) * 60.))
+        self.set_arcs((abs_all_d - self.d) * 3600. - self.arcm * 60.)
+
+    def converter(self):
+        if self.priority == 0:
+            self.unit()
+        if self.priority == -2:
+            # The d:m:s form exists
+            self.dms2all_d()
+            # If d and m in d:m:s are not integers, recalc the d:m:s form.
+            d = self.d
+            arcm = self.arcm
+            if d % 1 != 0. or arcm % 1 != 0.:
+                self.all_d2dms()
+            else:
+                self.set_d(int(d))
+                self.set_arcm(int(arcm))
+        else:
+            # The degree form exists
+            self.all_d2dms()
+
+
+class Coor(object):
+
+    '''
+    A wrapper class contains a pair of ra and dec classes.
+        e.g. Coor(input_ra, input_dec)
+    input_ra/dec could be:
+        1. strings of RA (h:m:s/d:m:s/degree) and Dec (d:m:s/degree)
+        2. floats of RA and Dec in pure degree forms
+        3. the ndarray elements return from astropy.wcs.all_pix2world()
+        4. ra class/dec class objects
+    While print a Coor class, 'h:m:s, d:m:s' will show up.
+    '''
+    def __init__(self, input_ra, input_dec):
+        if isinstance(input_ra, basestring):
+            self.RA = resolve_RA(input_ra)
+        elif isinstance(input_ra, float):
+            self.RA = ra(all_d=input_ra)
+        elif isinstance(input_ra, np.ndarray) \
+                and len(np.shape(input_ra)) == 0:
+            # For astropy.wcs
+            self.RA = ra(all_d=np.asscalar(input_ra))
+        else:
+            self.RA = input_ra
+        if isinstance(input_dec, basestring):
+            self.Dec = resolve_Dec(input_dec)
+        elif isinstance(input_dec, float):
+            self.Dec = dec(all_d=input_dec)
+        elif isinstance(input_dec, np.ndarray) \
+                and len(np.shape(input_dec)) == 0:
+            # For astropy.wcs
+            self.Dec = dec(all_d=np.asscalar(input_dec))
+        else:
+            self.Dec = input_dec
+        self.RA.converter()
+        self.Dec.converter()
+
+    def converter(self):
+        self.RA.converter()
+        self.Dec.converter()
+
+    def __repr__(self):
+        return '{0}, {1}'.format(self.RA.__repr__(), self.Dec.__repr__())
+
+
+def converter(co):
+
+    '''
+    Calculate all the representation of a Coor/ra/dec class.
+    '''
+    co.converter()
+
+
+def resolve_RA(string):
+
+    '''
+    Analysis the string of RA in either h:m:s, 'd'd:m:s, or degree forms.
+        e.g. '4:31:30', 'd67:52:30', or '67.875'.
+    Note that the dms form should be prefixed by 'd'.
+    '''
+    RA = ra()
+    try:
+        if 'd' in string:
+            tmp = string.split('d')
+            tmp = tmp[1].split(':')
+            RA.d = float(tmp[0])
+            RA.arcm = float(tmp[1])
+            RA.arcs = float(tmp[2])
+        elif ':' in string:
+            tmp = string.split(':')
+            RA.h = float(tmp[0])
+            RA.m = float(tmp[1])
+            RA.s = float(tmp[2])
+        else:
+            RA.all_d = float(string)
+        RA.converter()
+    except:
+        raise ValueError('Please check your RA forms!')
+    return RA
+
+
+def resolve_Dec(string):
+
+    '''
+    Analysis the string of Dec in either h:m:s, d:m:s, or degree forms.
+        e.g. '+18:12:30', or '+18.208333'
+    '''
+    Dec = dec()
+    try:
+        if ':' in string:
+            tmp = string.split(':')
+            if '-' in tmp[0]:
+                Dec.sign = '-'
+            if '+' in tmp[0]:
+                tmp[0].replace('+', '')
+            Dec.d = abs(float(tmp[0]))
+            Dec.arcm = float(tmp[1])
+            Dec.arcs = float(tmp[2])
+        else:
+            Dec.all_d = float(string)
+        Dec.converter()
+    except:
+        raise ValueError('Please check your Dec forms!\n'
+                'If there are +/- signs, '
+                'the sign and number can\'t be separated by space.')
+    return Dec
+
+# The marks for DistanceMode() and DisplacementMode():
+#      C          N
+#     / \         ^
+#  aa/   \bb      |
+#   /     \   E<--
+#  /   cc  \
+# B - - - - A
+#(no. 2)    (no. 1)
+
+def CosineLaw_side(aa, bb, C):
+
+    '''
+    Get cc which belongs to [0, pi]
+    '''
+    cos_cc = np.cos(aa) * np.cos(bb) + np.sin(aa) * np.sin(bb) * np.cos(C)
+    return np.arccos(cos_cc)
+
+def CosineLaw_angle(aa, bb, cc):
+
+    '''
+    Get C which belongs to [0, pi]
+    '''
+    if aa == 0. or bb == 0.:
+        # aa = 0 => A = 0: point C == point B
+        # or bb = 0 => B = 0: point C == point A
+        return np.nan
+    else:
+        # aa != 0 and bb != 0
+        cos_C = \
+            (np.cos(cc) - np.cos(aa) * np.cos(bb)) / (np.sin(aa) * np.sin(bb))
+        if -1. <= cos_C <= 1.:
+            # Normal case: A = (0, pi) and B = (0, pi)
+            return np.arccos(cos_C)
+        else:
+            # (1) cc = aa + bb <=> A = 0, B = 0; C = pi
+            # (2) bb = aa + cc <=> A = 0, B = pi; C = 0
+            # (3) aa = bb + cc <=> A = pi, B = 0; C = 0
+            raise ValueError('Please check if (A=0 and B=0), (A=0 and B=pi), '
+                             'or (A=pi and B=0).')
+
+def DistanceMode(*args):
+
+    '''
+    DistanceMode(ra_p1, dec_p1, ra_p2, dec_p2)
+     or DistanceMode(Coor_p1, Coor_p2),
+     where ra_p#, dec_p#, and Coor_p# are ra/dec/Coor classes,
+    calculate the angular distance between p1 and p2.
+
+    Return a list:
+    [PA of p2 wrt p1[deg], PA of p1 wrt p2[deg], dist(p1, p2)[deg]]
+    '''
+    if len(args) == 4:
+        # args = ra(), dec(), ra(), dec()
+        ra_1 = args[0]
+        dec_1 = args[1]
+        ra_2 = args[2]
+        dec_2 = args[3]
+    elif len(args) == 2:
+        # args = Coor(), Coor()
+        ra_1 = args[0].RA
+        dec_1 = args[0].Dec
+        ra_2 = args[1].RA
+        dec_2 = args[1].Dec
+    else:
+        raise ValueError('Please check input for DistanceMode!')
+    # Calculate all forms of RA and Dec
+    for i in [ra_1, dec_1, ra_2, dec_2]:
+        i.converter()
+
+    # Transfer the orgin of Dec to be North pole
+    # and obtain aa, bb, and C in rad.
+    aa = (90. - dec_2.all_d) * deg2rad
+    bb = (90. - dec_1.all_d) * deg2rad
+    C = (ra_2.all_d - ra_1.all_d) * deg2rad
+    # Obtain A, B, cc; all in rad
+    cc = CosineLaw_side(aa, bb, C)
+    if cc == 0.:
+        # <=> aa = bb, C = 0
+        # point A == point B
+        A = np.nan
+        B = np.nan
+    else:
+        if C == 0.:
+            if aa > bb:
+                A = np.pi
+                B = 0.
+            elif bb > aa:
+                A = 0.
+                B = np.pi
+        else:
+            A = CosineLaw_angle(bb, cc, aa)
+            B = CosineLaw_angle(aa, cc, bb)
+            # Determine the direction of PA
+            A = A if C > 0. else -A
+            B = B if C < 0. else -B
+    return [A * rad2deg, B * rad2deg, cc * rad2deg]
+
+def DisplacementMode(*args):
+
+    '''
+    DisplacementMode(ra_p1, dec_p1, x["], pa[deg])
+     or DisplacementMode(Coor_p1, x["], pa[deg]),
+     where ra_p1, dec_p1 are ra/dec classes,
+    calculate the displaced point [ra_p2, dec_p2].
+
+    Return:
+    [ra_p2, dec_p2] or Coor(ra_p2, dec_p2)
+    '''
+    if len(args) == 4:
+        # args = ra(), dec(), x, pa
+        ra_p1 = args[0]
+        dec_p1 = args[1]
+    elif len(args) == 3:
+        # args = Coor(), x, pa
+        ra_p1 = args[0].RA
+        dec_p1 = args[0].Dec
+    else:
+        raise ValueError('Please check input for DisplacementMode!')
+    x = args[-2]
+    pa = args[-1]
+    # Calculate all forms of RA and Dec
+    for i in [ra_p1, dec_p1]:
+        i.converter()
+
+    # Obtain A, bb, cc; all in rad
+    A = pa * deg2rad
+    bb = (90. - dec_p1.all_d) * deg2rad
+    cc = x / 3600. * deg2rad
+    # Obtain aa, C; all in rad
+    if cc == 0.:
+        aa = bb
+        C = 0.
+    else:
+        aa = CosineLaw_side(bb, cc, A)
+        if pa % 180. == 0.:
+            C = 0.
+        else:
+            C = CosineLaw_angle(aa, bb, cc)
+            towardE = True if np.sin(A) > 0. else False
+            C = C if towardE else -C
+    # Calculate all forms of RA and Dec of the result
+    ra_p2 = ra()
+    dec_p2 = dec()
+    ra_p2.all_d = ra_p1.all_d + C * rad2deg
+    ra_p2.converter()
+    dec_p2.all_d = 90. - aa * rad2deg
+    dec_p2.converter()
+    if len(args) == 4:
+        return [ra_p2, dec_p2]
+    if len(args) == 3:
+        return Coor(ra_p2, dec_p2)
